8a8dbbe52ff336a6c191efd487c1b7de64900090
prernamoreit-coder <prerna_more_it@moderncoe.edu.in>
2026-04-26 16:14:59 +0530

    Fix exact categorical values + minimal input

--- app.py
+++ app.py
@@ -18,9 +18,13 @@ salary_range = st.text_input("Salary Range", value="Missing")
 company_profile = st.text_area("Company Profile", value="Missing")
 requirements = st.text_area("Requirements", value="Missing")
 benefits = st.text_area("Benefits", value="Missing")
-employment_type = st.selectbox("Employment Type", ["Missing", "Full-time", "Other", "Contract", "Temporary"])
-required_experience = st.selectbox("Required Experience", ["Missing", "Not Applicable", "Entry level", "Internship", "Mid-Senior level"])
-required_education = st.selectbox("Required Education", ["Missing", "Bachelor's Degree", "High School", "Professional"])
+employment_type = st.selectbox("Employment Type", 
+                              ["Full-time", "Other", "Contract", "Temporary", "Part-time"])
+required_experience = st.selectbox("Required Experience", 
+                                 ["Not Applicable", "Entry level", "Internship", "Mid-Senior level", "Executive", "Associate", "Senior level"])
+
+required_education = st.selectbox("Required Education", 
+                                 ["Bachelor's Degree", "High School or equivalent", "Doctorate", "Master's Degree", "Professional", "College Degree", "Vocational", "Certification", "Associate Degree"])
 industry = st.text_input("Industry", value="Missing")
 function = st.text_input("Function", value="Missing")
 
